cb33e7d70f51dac7c52b46677189e5670351c55a
joseyzambranov <joseyzambranovpe@gmail.com>
2022-07-16 13:52:42 -0500

    added mongomon and test get api/convert  passed

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,15 +1,60 @@
+function numberStringSplitter(input){
+  let number = input.match(/[.\d\/]+/g)||["1"];
+  let string = input.match(/[a-zA-Z]+/g)[0];
+
+  return [number[0],string];
+}
+
+function checkDIv(possibleFraction){
+  let nums = possibleFraction.split("/");
+  if(nums.length>2){
+    return false;
+  }
+  return nums
+}
+
+
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
+    let result=numberStringSplitter(input)[0];
+    let nums=checkDIv(result)
+    if(!nums){
+      return undefined;
+    }
+
+    let num1=nums[0];
+    let num2=nums[1]||"1";
+
+    result = parseFloat(num1)/parseFloat(num2);
+
+    if(isNaN(num1)||isNaN(num2)){
+      return undefined;
+    }
     
     return result;
   };
   
   this.getUnit = function(input) {
-    let result;
+    let result=numberStringSplitter(input)[1].toLowerCase();
+
+    switch(result){
+      case "km":
+        return "km";
+      case "gal":
+        return "gal";
+      case "lbs":
+        return "lbs";
+      case "mi":
+        return "mi";
+      case "l":
+        return "L";  
+      case "kg":
+        return "kg";
+      default:
+        return undefined;        
+    }
     
-    return result;
   };
   
   this.getReturnUnit = function(initUnit) {
