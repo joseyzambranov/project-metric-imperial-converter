62d919fd5bad698f21596e806a1f6a1ef1c4346a
joseyzambranov <joseyzambranovpe@gmail.com>
2022-07-16 19:51:37 -0500

    pass 11 test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -30,8 +30,9 @@ suite('Functional Tests', function() {
                 .end(function(err,res){
                     assert.equal(res.status,200);
                     assert.equal(res.body.initUnit,undefined);
+                    done()
                 })
-                done()
+                
 
             })
             test("Convert 3/7.2/4kg (invalid number)",function(done){
